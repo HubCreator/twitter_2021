de35b8c04469563f064b6f18b9c6a4e92bd1e9d8
HyunSeong Jeong <chocomilk8604@gmail.com>
2021-07-06 23:43:05 +0900

    Can change username of current user on profile page

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,61 @@
-import { authService } from "my_firebase";
-import React from "react";
+import { authService, dbService } from "my_firebase";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+const Profile = ({ userObj }) => {
+  const [updateName, setUpdateName] = useState(userObj.displayName);
   const history = useHistory();
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyTweets = async () => {
+    const tweets = await dbService
+      .collection("tweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+
+    console.log(tweets.docs.map((element) => element.data()));
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setUpdateName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (updateName !== userObj.displayName) {
+      await userObj.updateProfile({
+        displayName: updateName,
+      });
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          name="name"
+          value={updateName}
+          type="text"
+          placeholder="Display name"
+          onChange={onChange}
+        />
+        <input type="submit" placeholder="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
 };
+
+export default Profile;
